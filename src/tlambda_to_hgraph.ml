4b426871374198d033134348be4f4c5cf246f115
Thomas Blanc <thomas.blanc@ens-cachan.fr>
2013-08-13 17:33:13 +0200

    Hedges are now ints, they can contain several (possibly mutually recursive) definitions

--- src/tlambda_to_hgraph.ml
+++ src/tlambda_to_hgraph.ml
@@ -1,6 +1,15 @@
 type id = Ident.t
 
-module Vertex =
+module type E =
+sig
+  type t
+  val compare : t -> t -> int
+  val equal : t -> t -> bool
+  val hash : t -> int
+  val mk : unit -> t
+end
+
+module Vertex : E =
 struct
   type t = int
   let compare (x:int) y = compare x y
@@ -11,12 +20,15 @@ struct
   let mk () = incr c; !c
 end
 
-module Hedge =
+module Hedge : E =
 struct
-  type t = id
-  let compare = compare
-  let equal = (=)
-  let hash x = Hashtbl.hash x.Ident.stamp
+  type t = int
+  let compare (x:int) y = compare x y
+  let equal (x:int) y = x = y
+  let hash (x:int) = Hashtbl.hash x
+
+  let c = ref 0
+  let mk () = incr c; !c
 end
 
 module Desc =
@@ -59,6 +71,11 @@ let mk_graph ~last_id ~funs main =
   in
   let open G in
   let g = create () in
+
+  let simpleh id v ~inv ~outv =
+    add_hedge g ( Hedge.mk ()) [id,v] ~pred:[|inv|] ~succ:[|outv|]
+  in
+
   let nv () =
     let v = Vertex.mk () in
     add_vertex g v (); v in
@@ -79,7 +96,7 @@ let mk_graph ~last_id ~funs main =
     match code with
     | Tlet d -> tlet inv outv exnv ret_id exn_id d
     | Trec d -> trec inv outv exnv ret_id exn_id d
-    | Tend id -> add_hedge g ret_id ( Var id) ~pred:[|inv|] ~succ:[|outv|]
+    | Tend id -> simpleh ret_id ( Var id) ~inv ~outv
 
   and tlet inv outv exnv ret_id exn_id d =
     (* add_vertex g d.te_id (); *)
@@ -91,23 +108,21 @@ let mk_graph ~last_id ~funs main =
 
   and tcontrol inv outv exnv id ret_id exn_id c =
     match c with
-    | Tvar i -> add_hedge g id ( Var i) ~pred:[|inv|] ~succ:[|outv|]
+    | Tvar i -> simpleh id ( Var i) ~inv ~outv
 
-    | Tconst c -> add_hedge g id ( Const c) ~pred:[|inv|] ~succ:[|outv|]
+    | Tconst c -> simpleh id ( Const c) ~inv ~outv
 
     | Tapply ( f, x, _) ->
-      let fv = nv () in
-      add_hedge g fun_id ( Var f) ~pred:[|inv|] ~succ:[|fv|];
-      add_hedge g f_arg_id ( Var x) ~pred:[|fv|] ~succ:fun_in;
-      add_hedge g exn_id ( Var f_exn_id) ~pred:fun_exn ~succ:[|exnv|];
-      add_hedge g id ( Var f_ret_id) ~pred:fun_out ~succ:[|outv|]
+      add_hedge g ( Hedge.mk ()) [f_arg_id, ( Var x); fun_id, ( Var f)] ~pred:[|inv|] ~succ:fun_in;
+      add_hedge g ( Hedge.mk ()) [exn_id, ( Var f_exn_id)] ~pred:fun_exn ~succ:[|exnv|];
+      add_hedge g ( Hedge.mk ()) [id, ( Var f_ret_id)] ~pred:fun_out ~succ:[|outv|]
 
-    | Tprim ( p, args) -> add_hedge g id ( Prim ( p, args)) ~pred:[|inv|] ~succ:[|outv|]
+    | Tprim ( p, args) -> simpleh id ( Prim ( p, args)) ~inv ~outv
 
     | Tswitch ( si_id, s) ->
       let switch_handle is_cp (i,lam) =
 	let inc = nv () in
-	add_hedge g si_id ( Constraint ( if is_cp then Ccp i else Ctag i)) ~pred:[|inv|] ~succ:[|inc|];
+	simpleh si_id ( Constraint ( if is_cp then Ccp i else Ctag i)) ~inv ~outv:inc;
 	tlambda ~outv ~ret_id ~inv:inc ~exnv ~exn_id lam
       in
       let () = List.iter ( switch_handle true) s.t_consts
@@ -130,22 +145,15 @@ let mk_graph ~last_id ~funs main =
 	  let cps = get_not_used s.t_numconsts s.t_consts
 	  and bs = get_not_used s.t_numblocks s.t_blocks in
 	  let inf = nv () in
-	  Is.iter (fun cp -> add_hedge g si_id (Constraint (Ccp cp)) ~pred:[|inv|] ~succ:[|inf|]) cps;
-	  Is.iter (fun tag -> add_hedge g si_id (Constraint (Ctag tag)) ~pred:[|inv|] ~succ:[|inf|]) bs;
+	  Is.iter (fun cp -> simpleh si_id (Constraint (Ccp cp)) ~inv ~outv:inf) cps;
+	  Is.iter (fun tag -> simpleh si_id (Constraint (Ctag tag)) ~inv ~outv:inf) bs;
 	  tlambda ~outv ~ret_id ~inv:inf ~exnv ~exn_id lam
       end
 
     | Tstaticraise ( i, args) ->
       let ( catchv, cargs) = Hashtbl.find statics i in
-      let endv =
-	List.fold_left2 
-	  (fun v carg arg ->
-	    let v2 = nv () in
-	    add_hedge g carg ( Var arg) ~pred:[|v2|] ~succ:[|v|];
-	    v2)
-	  inv ( List.tl cargs) ( List.tl args)
-      in
-      add_hedge g ( List.hd cargs) (Var ( List.hd args)) ~pred:[|endv|] ~succ:[|catchv|]
+      let assigns = List.map2 (fun carg arg -> ( carg, Var arg)) cargs args in
+      add_hedge g ( Hedge.mk ()) assigns ~pred:[|inv|] ~succ:[|catchv|]
 
     | Tstaticcatch ( ltry, ( i, args), lwith) ->
       let catchv = nv () in
@@ -153,7 +161,7 @@ let mk_graph ~last_id ~funs main =
       tlambda ~outv ~ret_id ~inv ~exnv ~exn_id ltry;
       tlambda ~outv ~ret_id ~inv:catchv ~exnv ~exn_id lwith
       
-    | Traise i -> add_hedge g exn_id (Var i) ~pred:[|inv|] ~succ:[|exnv|]
+    | Traise i -> simpleh exn_id (Var i) ~inv ~outv:exnv
 
     | Ttrywith ( ltry, exni, lwith)  ->
       let exnv2 = nv () in
@@ -163,16 +171,16 @@ let mk_graph ~last_id ~funs main =
     | Tifthenelse ( i, t, e) ->
       let int = nv ()
       and ine = nv () in
-      add_hedge g i ctrue ~pred:[|inv|] ~succ:[|int|];
-      add_hedge g i cfalse ~pred:[|inv|] ~succ:[|ine|];
+      simpleh i ctrue ~inv ~outv:int;
+      simpleh i cfalse ~inv ~outv:ine;
       tlambda ~ret_id ~exn_id ~inv:int ~outv ~exnv t;
       tlambda ~ret_id ~exn_id ~inv:ine ~outv ~exnv e
 
     | Twhile ( lcond, lbody) ->
       let outc = nv () in
       let inb = nv () in
-      add_hedge g ret_id cfalse ~pred:[|outc|] ~succ:[|outv|];
-      add_hedge g ret_id ctrue ~pred:[|outc|] ~succ:[|inb|];
+      simpleh ret_id cfalse ~inv:outc ~outv;
+      simpleh ret_id ctrue ~inv:outc ~outv:inb;
       tlambda ~outv:outc ~ret_id ~exn_id ~inv ~exnv lcond;
       tlambda ~outv:inv ~ret_id ~exn_id ~inv:inb ~exnv lbody
 
@@ -182,11 +190,11 @@ let mk_graph ~last_id ~funs main =
       let testv = nv () in
       let inb = nv () in
       let outb = nv () in
-      add_hedge g i ( Var start) ~pred:[|inv|] ~succ:[|initv|];
-      add_hedge g test_id ( Prim ( Pintcomp Lambda.Cle, [i;stop])) ~pred:[|initv|] ~succ:[|testv|];
-      add_hedge g test_id ctrue ~pred:[|testv|] ~succ:[|inb|];
-      add_hedge g i ( Prim ( Paddint, [i;one_id])) ~pred:[|outb|] ~succ:[|initv|];
-      add_hedge g test_id cfalse ~pred:[|testv|] ~succ:[|outv|];
+      simpleh i ( Var start) ~inv ~outv:initv;
+      simpleh test_id ( Prim ( Pintcomp Lambda.Cle, [i;stop])) ~inv:initv ~outv:testv;
+      simpleh test_id ctrue ~inv:testv ~outv:inb;
+      simpleh i ( Prim ( Paddint, [i;one_id])) ~inv:outb ~outv:initv;
+      simpleh test_id cfalse ~inv:testv ~outv;
       tlambda ~outv:outb ~ret_id ~exn_id ~inv:inb ~exnv lbody
   in
   let inv = nv () and outv = nv () and exnv = nv () in
