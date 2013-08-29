1a2589becaf04e6bdf77d16d02cef170616edaca
Thomas Blanc <thomas.blanc@ens-cachan.fr>
2013-08-29 18:50:42 +0200

    Indentation

--- src/tlambda-analysis/tlambda_analysis.ml
+++ src/tlambda-analysis/tlambda_analysis.ml
@@ -81,140 +81,140 @@ struct
 
   let apply (_ :hedge ) ( l : hedge_attribute ) ( envs : abstract array ) =
   let constant _ = failwith "TODO: constant !" in
-    let in_apply ( id, action) env =
-      let set x = set_env id x env
-      and get x = get_env x env
-      and vunit = cp_singleton 0
-      in
-      match action with
-      | App _ -> assert false
-      | Var i -> set ( get i)
-      | Const c -> set ( constant c)
-      | Prim ( p, l,exnid) ->
-	begin
-	  match p, l with
-	  | TPidentity, [i] -> set ( get i)
-	  | TPignore, _ -> set vunit
+  let in_apply ( id, action) env =
+    let set x = set_env id x env
+    and get x = get_env x env
+    and vunit = cp_singleton 0
+    in
+    match action with
+    | App _ -> assert false
+    | Var i -> set ( get i)
+    | Const c -> set ( constant c)
+    | Prim ( p, l,exnid) ->
+      begin
+	match p, l with
+	| TPidentity, [i] -> set ( get i)
+	| TPignore, _ -> set vunit
 	  (* Operations on heap blocks *)
-	  | TPmakeblock ( tag, _), _ ->
-	    let a = Array.of_list l in
-	    set ( block_singleton tag a )
-	  | TPfield i, [b] ->
-	    let env = set_env b ( restrict_block ~has_field:i ( get b)) env in
-	    set ( get_field i ( get_env b env) env) (* must restrict b to a block with field at least i *)
-(*	  | TPsetfield ( i, _), [b;v] ->
-	    let env = set_env b ( set_field i v ( get b)) env in
-	    set_env id vunit env *)
-	  | TPfloatfield i, [b] -> failwith "TODO: floatfield"
-	  | TPsetfloatfield i, [b;v] -> failwith "TODO: setfloatfield"
-	  | TPduprecord (trepr,i), [r] -> failwith "TODO: duprecord"
+	| TPmakeblock ( tag, _), _ ->
+	  let a = Array.of_list l in
+	  set ( block_singleton tag a )
+	| TPfield i, [b] ->
+	  let env = set_env b ( restrict_block ~has_field:i ( get b)) env in
+	  set ( get_field i ( get_env b env) env) (* must restrict b to a block with field at least i *)
+	  (*	  | TPsetfield ( i, _), [b;v] ->
+		  let env = set_env b ( set_field i v ( get b)) env in
+		  set_env id vunit env *)
+	| TPfloatfield i, [b] -> failwith "TODO: floatfield"
+	| TPsetfloatfield i, [b;v] -> failwith "TODO: setfloatfield"
+	| TPduprecord (trepr,i), [r] -> failwith "TODO: duprecord"
 	  (* Force lazy values *)
-	  | TPlazyforce, _ -> failwith "TODO: Force lazy"
+	| TPlazyforce, _ -> failwith "TODO: Force lazy"
 	  (* External call *)
-	  | TPccall prim, _ -> failwith "TODO: C-call"
+	| TPccall prim, _ -> failwith "TODO: C-call"
 	  (* Boolean operations *)
-	  | TPnot, [i] -> set ( not_bool ( get i))
+	| TPnot, [i] -> set ( not_bool ( get i))
 	  (* Integer operations *)
-	  | TPnegint, [i] -> set ( int_op1 uminus ( get i))
-	  | TPaddint, [x;y]
-	  | TPsubint, [x;y]
-	  | TPmulint, [x;y]
-	  | TPdivint, [x;y]
-	  | TPmodint, [x;y]
-	  | TPandint, [x;y]
-	  | TPorint, [x;y]
-	  | TPxorint, [x;y]
-	  | TPlslint, [x;y]
-	  | TPlsrint, [x;y]
-	  | TPasrint, [x;y] -> set ( int_op2 ( intop2_of_prim p) (get x) (get y))
-	  | TPintcomp c, [x;y] -> 
-	    let res, x', y' = int_comp c ( get x) ( get y) in
-	    set_env x x' ( set_env y y' ( set res ) )
-(*	  | TPoffsetint of int
-	  | TPoffsetref of int
+	| TPnegint, [i] -> set ( int_op1 uminus ( get i))
+	| TPaddint, [x;y]
+	| TPsubint, [x;y]
+	| TPmulint, [x;y]
+	| TPdivint, [x;y]
+	| TPmodint, [x;y]
+	| TPandint, [x;y]
+	| TPorint, [x;y]
+	| TPxorint, [x;y]
+	| TPlslint, [x;y]
+	| TPlsrint, [x;y]
+	| TPasrint, [x;y] -> set ( int_op2 ( intop2_of_prim p) (get x) (get y))
+	| TPintcomp c, [x;y] -> 
+	  let res, x', y' = int_comp c ( get x) ( get y) in
+	  set_env x x' ( set_env y y' ( set res ) )
+	  (*	  | TPoffsetint of int
+		  | TPoffsetref of int
 	  (* Float operations *)
-	  | TPintoffloat | TPfloatofint
-	  | TPnegfloat | TPabsfloat
-	  | TPaddfloat | TPsubfloat | TPmulfloat | TPdivfloat
-	  | TPfloatcomp of comparison
+		  | TPintoffloat | TPfloatofint
+		  | TPnegfloat | TPabsfloat
+		  | TPaddfloat | TPsubfloat | TPmulfloat | TPdivfloat
+		  | TPfloatcomp of comparison
 	  (* String operations *)
-	  | TPstringlength | TPstringrefu | TPstringsetu | TPstringrefs | TPstringsets
+		  | TPstringlength | TPstringrefu | TPstringsetu | TPstringrefs | TPstringsets
 	  (* Array operations *)
-	  | TPmakearray of array_kind
-	  | TParraylength of array_kind
-	  | TParrayrefu of array_kind
-	  | TParraysetu of array_kind
-	  | TParrayrefs of array_kind
-	  | TParraysets of array_kind
+		  | TPmakearray of array_kind
+		  | TParraylength of array_kind
+		  | TParrayrefu of array_kind
+		  | TParraysetu of array_kind
+		  | TParrayrefs of array_kind
+		  | TParraysets of array_kind
 	  (* Test if the argument is a block or an immediate integer *)
-	  | TPisint
+		  | TPisint
 	  (* Test if the (integer) argument is outside an interval *)
-	  | TPisout
+		  | TPisout
 	  (* Bitvect operations *)
-	  | TPbittest
+		  | TPbittest
 	  (* Operations on boxed integers (Nativeint.t, Int32.t, Int64.t) *)
-	  | TPbintofint of boxed_integer
-	  | TPintofbint of boxed_integer
-	  | TPcvtbint of boxed_integer (*source*) * boxed_integer (*destination*)
-	  | TPnegbint of boxed_integer
-	  | TPaddbint of boxed_integer
-	  | TPsubbint of boxed_integer
-	  | TPmulbint of boxed_integer
-	  | TPdivbint of boxed_integer
-	  | TPmodbint of boxed_integer
-	  | TPandbint of boxed_integer
-	  | TPorbint of boxed_integer
-	  | TPxorbint of boxed_integer
-	  | TPlslbint of boxed_integer
-	  | TPlsrbint of boxed_integer
-	  | TPasrbint of boxed_integer
-	  | TPbintcomp of boxed_integer * comparison
+		  | TPbintofint of boxed_integer
+		  | TPintofbint of boxed_integer
+		  | TPcvtbint of boxed_integer (*source*) * boxed_integer (*destination*)
+		  | TPnegbint of boxed_integer
+		  | TPaddbint of boxed_integer
+		  | TPsubbint of boxed_integer
+		  | TPmulbint of boxed_integer
+		  | TPdivbint of boxed_integer
+		  | TPmodbint of boxed_integer
+		  | TPandbint of boxed_integer
+		  | TPorbint of boxed_integer
+		  | TPxorbint of boxed_integer
+		  | TPlslbint of boxed_integer
+		  | TPlsrbint of boxed_integer
+		  | TPasrbint of boxed_integer
+		  | TPbintcomp of boxed_integer * comparison
 	  (* Operations on big arrays: (unsafe, #dimensions, kind, layout) *)
-	  | TPbigarrayref of bool * int * bigarray_kind * bigarray_layout
-	  | TPbigarrayset of bool * int * bigarray_kind * bigarray_layout
+		  | TPbigarrayref of bool * int * bigarray_kind * bigarray_layout
+		  | TPbigarrayset of bool * int * bigarray_kind * bigarray_layout
 	  (* size of the nth dimension of a big array *)
-	  | TPbigarraydim of int
+		  | TPbigarraydim of int
 	  (* load/set 16,32,64 bits from a string: (unsafe)*)
-	  | TPstring_load_16 of bool
-	  | TPstring_load_32 of bool
-	  | TPstring_load_64 of bool
-	  | TPstring_set_16 of bool
-	  | TPstring_set_32 of bool
-	  | TPstring_set_64 of bool
+		  | TPstring_load_16 of bool
+		  | TPstring_load_32 of bool
+		  | TPstring_load_64 of bool
+		  | TPstring_set_16 of bool
+		  | TPstring_set_32 of bool
+		  | TPstring_set_64 of bool
 	  (* load/set 16,32,64 bits from a
-	     (char, int8_unsigned_elt, c_layout) Bigarray.Array1.t : (unsafe) *)
-	  | TPbigstring_load_16 of bool
-	  | TPbigstring_load_32 of bool
-	  | TPbigstring_load_64 of bool
-	  | TPbigstring_set_16 of bool
-	  | TPbigstring_set_32 of bool
-	  | TPbigstring_set_64 of bool
+		  (char, int8_unsigned_elt, c_layout) Bigarray.Array1.t : (unsafe) *)
+		  | TPbigstring_load_16 of bool
+		  | TPbigstring_load_32 of bool
+		  | TPbigstring_load_64 of bool
+		  | TPbigstring_set_16 of bool
+		  | TPbigstring_set_32 of bool
+		  | TPbigstring_set_64 of bool
 	  (* Compile time constants *)
-	  | TPctconst of compile_time_constant
+		  | TPctconst of compile_time_constant
 	  (* byte swap *)
-	  | TPbswap16
-	  | TPbbswap of boxed_integer
+		  | TPbswap16
+		  | TPbbswap of boxed_integer
 	  (* method call *)
-	  | Method_send of Lambda.meth_kind * Location.t (* moved from lambda to primitive *)
-*)	      
-	  | _ -> failwith "TODO: primitives !"
-	end
-      | Constraint c ->
-	begin
-	  match c with
-	  | Ccp v  -> set ( restrict_cp ~v (get_env id env))
-	  | Ctag tag -> set ( restrict_block ~tag ( get_env id env ) )
-	end
-    in	
-    let env = Array.fold_left join_env bottom_env envs in
-    let rec aux e l =
+		  | Method_send of Lambda.meth_kind * Location.t (* moved from lambda to primitive *)
+	  *)	      
+	| _ -> failwith "TODO: primitives !"
+      end
+    | Constraint c ->
+      begin
+	match c with
+	| Ccp v  -> set ( restrict_cp ~v (get_env id env))
+	| Ctag tag -> set ( restrict_block ~tag ( get_env id env ) )
+      end
+  in	
+  let env = Array.fold_left join_env bottom_env envs in
+  let rec aux e l =
     match l with
     | [] -> e
     | h :: t -> aux (in_apply h e) t
-    in 
-    match l with
-    | [ id, ( App ( f, x ) ) ] ->
-      ( [| set_env id (get_env E.return_id env) env; env |], ( fun_ids f env ) )
-    | _ -> [|aux env l|], []
+  in 
+  match l with
+  | [ id, ( App ( f, x ) ) ] ->
+    ( [| set_env id (get_env E.return_id env) env; env |], ( fun_ids f env ) )
+  | _ -> [|aux env l|], []
 
 end
