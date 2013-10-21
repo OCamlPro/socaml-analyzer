51d4328c53355d6389bfa407a217b807bf2a8402
Thomas Blanc <thomas.blanc@ens-cachan.fr>
2013-10-21 14:26:32 +0200

    Changing tlambda id, it does not compile at all, don't need to try

--- src/tlambda/tlambda.mli
+++ src/tlambda/tlambda.mli
@@ -1,13 +1,18 @@
 open Common_types
 
+type tid = string * id
+(* a tlambda id is a module * an id *)
+(* empty module means builtin or special *)
+(* "Module", { 0; "#self"; 0 } means a global module *)
+
 type tlambda =
 | Tlet of tlet
 | Trec of trec
-| Tend of id
+| Tend of tid
 
 and tlet =
 {
-  te_id : id;
+  te_id : tid;
   te_lam : tcontrol;
   te_kind : Lambda.let_kind;
   te_in : tlambda;
@@ -15,27 +20,27 @@ and tlet =
 
 and trec =
 {
-  tr_decls : ( id * primitive * id list ) list;
+  tr_decls : ( tid * primitive * tid list ) list;
   tr_in : tlambda;
 }
 
 and tcontrol =
-| Tvar of id
+| Tvar of tid
 | Tconst of Lambda.structured_constant
-| Tapply of id * id
-| Tprim of primitive * id list
-| Tswitch of id * tswitch
-| Tstaticraise of int * id list
-| Tstaticcatch of tlambda * (int * id list) * tlambda
-| Traise of id
-| Ttrywith of tlambda * id * tlambda
-| Tifthenelse of id * tlambda * tlambda
+| Tapply of tid * tid
+| Tprim of primitive * tid list
+| Tswitch of tid * tswitch
+| Tstaticraise of int * tid list
+| Tstaticcatch of tlambda * (int * tid list) * tlambda
+| Traise of tid
+| Ttrywith of tlambda * tid * tlambda
+| Tifthenelse of tid * tlambda * tlambda
 | Twhile of tlambda * tlambda
-| Tfor of id * id * id * direction_flag * tlambda
-(* | Tassign of id * id *)
-| Tlazyforce of id
-| Tccall of Primitive.description * id list
-| Tsend of Lambda.meth_kind * id * id
+| Tfor of tid * tid * tid * direction_flag * tlambda
+(* | Tassign of tid * tid *)
+| Tlazyforce of tid
+| Tccall of Primitive.description * tid list
+| Tsend of Lambda.meth_kind * tid * tid
 
 and tswitch =
 {
