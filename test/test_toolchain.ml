3604391fffebb90fa874c7afa09f12ad2ad07191
Thomas Blanc <thomas.blanc@ens-cachan.fr>
2013-11-18 17:52:51 +0100

    Adding -open to ttoolchain

--- test/test_toolchain.ml
+++ test/test_toolchain.ml
@@ -1,11 +1,23 @@
-let () =
-  Clflags.nopervasives := true;
-  Clflags.no_std_include := true
+let arg_parser =
+  let open Arg in
+  [
+    ( "-open",
+      String Mk_lambda.open_module,
+      "Add an implicitly opened module" )
+  ]
+
+let lamq = Queue.create ()
+
+let () = Arg.parse
+    arg_parser
+    (fun sourcefile ->
+       let lm = Mk_lambda.mk_lambda Format.std_formatter sourcefile in
+       Queue.add lm lamq
+    )
+    "please specify your .ml or .cmt files, order matters"
 
 let lambdas =
-  Mk_lambda.mk_lambdas
-    Format.std_formatter
-    ( Array.sub Sys.argv 1 ( pred ( Array.length Sys.argv ) ) )
+  Array.init ( Queue.length lamq ) (fun _ -> Queue.pop lamq)
 
 let () =  print_endline "Got lambdas !"
 
